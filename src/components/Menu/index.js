58e860c2ffdd7f8848182852aa899bbd5dd8622a
lucas ismael <lucasismael03@gmail.com>
2020-07-29 16:15:22 -0300

    v2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import Logo from  '../../assets/img/Logo.png'
-import './Menu.css'
-//import ButtonLink from './ButtonLink';
-import Button from '../Button'
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <a href="/">
-            <img className="Logo" src={Logo} alt="DevFlix "/>
-            </a>
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </a>
 
-            <Button as="a" href="/">
-            Novo vídeo
-            </Button>
-        </nav>
-    );
+      <Button as="a" className="ButtonLink" href="/">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
