618441f558c361304a556e0f04d5d850fe508caf
lucas ismael <lucasismael03@gmail.com>
2020-08-02 01:24:47 -0300

    teste

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -33,7 +33,7 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if(window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias'; 
+      const URL = 'https://developerflix22.herokuapp.com'; 
       fetch(URL)
        .then(async (respostaDoServer) =>{
         if(respostaDoServer.ok) {
